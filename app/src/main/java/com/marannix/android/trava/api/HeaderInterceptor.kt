5487288f92a6ba12ecafbdb4d5916b6758336b65
Josh <onabanjotobi@gmail.com>
2019-12-23 18:43:13 +0000

    Pass current date to intercepter

--- app/src/main/java/com/marannix/android/trava/api/HeaderInterceptor.kt
+++ app/src/main/java/com/marannix/android/trava/api/HeaderInterceptor.kt
@@ -2,6 +2,8 @@ package com.marannix.android.trava.api
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HeaderInterceptor : Interceptor {
 
@@ -13,10 +15,11 @@ class HeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response = chain.run {
         val original = chain.request()
         val httpUrl = original.url()
+        val date = SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(Date())
         val newHttpUrl = httpUrl.newBuilder()
             .addQueryParameter("client_id", "SWA00OHBC5NTTITYJ4IIZUD341VN3LC1SVRDYBLA2UTFLMAH")
             .addQueryParameter("client_secret", "JKB42M30XXP2MM45ALJK3XM4UT0XWUNUWAPB4UGS4OWZ5CPD")
-            .addQueryParameter("v", "20191221").build()
+            .addQueryParameter("v", date).build()
         proceed(
             request()
                 .newBuilder()
